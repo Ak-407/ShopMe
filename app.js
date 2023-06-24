8705ab28bf267fa33e36386f46bb28a25e5e22ef
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-06-24 23:24:41 +0530

    first commit

--- app.js
+++ app.js
@@ -4,29 +4,75 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
+let multer = require("multer")
 
 const app = express();
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost:27017/user3DB",{ useNewUrlParser: true , useUnifiedTopology: true });
+// mongoose.connect("mongodb://localhost:27017/user3DB",{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://jsamaan:amaan123@cluster0.vz55wc0.mongodb.net/jsamaan?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true });
+let storage = multer.diskStorage({
+  destination:"public/photo/",
+  filename:(req,file,cb)=>{
+    // cb(null,Date.now(+file.originalname))
+    cb(null,file.originalname)
+  }
+})
+let upload = multer({
+  storage:storage
+}).single('file');
 
-
-app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-const UserSchema = mongoose.Schema({
+
+const File = new mongoose.Schema({
+  name:String,
   email:String,
-  password:String
+  phone:Number,
+  productName:String,
+  productPrice:Number,
+  image:String
+});
+const Skill = new mongoose.Schema({
+  name:String,
+  profession:String,
+  experience:Number,
+  email:String,
+  phone:Number,
+  price:Number
+});
+const UserSchema = mongoose.Schema({
+  username:String,
+  password:String,
 });
 
 
 
 
+const file = mongoose.model("file", File);
+const skill = mongoose.model("skill", Skill);
 const user = mongoose.model("user", UserSchema);
 
-app.get("/", function(req,res){
-    res.render("home");
+// const cart = [];
+app.get("/", function(req, res) {
+  file.find({}, function(err, foundItems) {
+    skill.find({}, function(err, skillItems) {
+      res.render("header", { recordss: foundItems, skillName: skillItems });
+    });
+  });
 });
+
+app.post("/",function(req,res){
+  res.render("header")
+  file.find({}, function(err, foundItems) {
+    skill.find({}, function(err, skillItems) {
+      res.render("header", { recordss: foundItems, skillName: skillItems });
+    });
+  });
+  search=req.body.search1
+  console.log(search)
+})
 
 app.get("/register", function(req,res){
   res.render("register");
@@ -36,25 +82,54 @@ app.get("/login", function(req,res){
   res.render("login");
 });
 
-app.post("/register", function(req,res){
-  const user1 = new user({
-    email:req.body.username,
-    password:md5(req.body.password)
+app.get("/submit",function(req,res){
+  res.render("sec1");
+})
+app.get("/adminn",function(req,res){
+  res.render("adminn");
+  
+})
+app.get("/skill",function(req,res){
+  res.render("skill");
+  
+})
+
+
+app.post("/register", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Validate username and password
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required.");
+  }
+
+  const newUser = new user({
+    username: username, // Make sure the username is mapped correctly
+    password: md5(password)
   });
-  user1.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }
-    else{
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("adminn");
+    } else {
       res.send(err);
     }
-  })
+  });
 });
+
+
+
 
 app.post("/login", function(req,res){
   const Username=req.body.username;
   const Password=md5(req.body.password);
-  user.findOne({email: Username},function(err, foundItems){
+  if(Username=="admin@gmail.com" && req.body.password=="123"){
+    res.render("adminn");
+  }else{
+    // res.send("err");
+  
+  user.findOne({username: Username},function(err, foundItems){
     console.log(foundItems);
     if(err){
       res.send("err");
@@ -64,7 +139,7 @@ app.post("/login", function(req,res){
       if(foundItems){
         console.log(foundItems);
         if(foundItems.password === Password){
-          res.render("secrets");
+          res.render("adminn");
         }else{
           res.send("err");
         }}else{
@@ -72,7 +147,45 @@ app.post("/login", function(req,res){
           res.send("err33");
 
         }
-      }})});
+      }})}});
+
+
+      app.post("/adminn", upload, function(req, res) {
+        const photo = new file({
+          name: req.body.name,
+          email: req.body.email,
+          phone:req.body.phone,
+          productName:req.body.productName,
+          productPrice:req.body.productPrice,
+          image: req.file.filename
+        })
+        photo.save(function(err, foundItems) {
+            if (!err) {
+                res.redirect("/"); // Redirect to the home page
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+    app.post("/skill", upload, function(req, res) {
+      const skillPerson = new skill({
+        name: req.body.name,
+        profession:req.body.profession,
+        experience:req.body.experience,
+        email: req.body.email,
+        phone:req.body.phone,
+        price:req.body.price
+      })
+      skillPerson.save(function(err, foundItems) {
+          if (!err) {
+              res.redirect("/"); // Redirect to the home page
+          } else {
+              res.send(err);
+          }
+      });
+  });
+    
 
         
 
@@ -81,6 +194,6 @@ app.post("/login", function(req,res){
 
 
 
-app.listen(3000, function() {
+app.listen(4000, function() {
   console.log("Server started on port 3000.");
 });
